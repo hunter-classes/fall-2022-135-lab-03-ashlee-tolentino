e0ce38338eaac8259c1c9a936f62b6cfcfd784e0
Ashlee Tolentino <ashleetolentino@Ashlees-Air.lan>
2022-09-17 00:46:43 -0400

    task A done

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
-// add your code as appropriate 
+/*
+Author: Ashlee Tolentino
+Course: CSCI-135
+Instructor: Michael Zamansky
+Assignment: Lab3A
+
+This program accepts a std::string specifying a date and it returns the East Basin storage for that day.
+*/
+
 #include <iostream>
+#include <string>
+
+#include <fstream>
+#include <cstdlib>
+#include <climits>
+
+#include "reservoir.h"
 
 int main()
 {
-  
-
-  
+  double east_storage = get_east_storage("05/20/2018");
+  std::cout << "East basin storage: " << east_storage << " billion gallons" << std::endl;
+ 
   return 0;
 }
