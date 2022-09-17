394cc88a9e970170cece2b9d3aad5ea50c23186e
Ashlee Tolentino <ashleetolentino@Ashlees-Air.lan>
2022-09-17 01:11:03 -0400

    task B done

--- reservoir.cpp
+++ reservoir.cpp
@@ -2,7 +2,7 @@
 Author: Ashlee Tolentino
 Course: CSCI-135
 Instructor: Michael Zamansky
-Assignment: Lab3A
+Assignment: Lab3B
 
 This program accepts a std::string specifying a date and it returns the East Basin storage for that day.
 */
@@ -16,6 +16,7 @@ This program accepts a std::string specifying a date and it returns the East Bas
 
 #include "reservoir.h"
 
+// This function accepts an std::string specifying a date and returns the East basin storage for that day
 double get_east_storage(std::string date)
 {
 	double eastStorage;
@@ -52,4 +53,84 @@ double get_east_storage(std::string date)
 
 	return eastStorage;
 	fin.close();
-}  
+}
+
+
+// Returns the minimum storage in the East basin in 2018
+double get_min_east()
+{
+	double minStorage = INT_MAX;
+	
+	std::string d;
+	double eastSt;
+	double eastEl;
+	double westSt;
+	double westEl;
+
+	std::ifstream fin("Current_Reservoir_Levels.tsv");
+	if(fin.fail())
+	{
+		std::cerr << "File cannot be opened for reading." << std::endl;
+		exit(1);	// exit if failed to open the file
+	}
+
+	std::string junk;	// new string variable
+	getline(fin, junk);	// read one line from the file
+  
+  	while(fin >> d >> eastSt >> eastEl >> westSt >> westEl)
+	{
+			// this loop reads the file line-by-line
+			// extracting 5 values on each iteration
+
+		fin.ignore(INT_MAX, '\n');	// skips to the end of line,
+													// ignoring the remaining columns
+
+		if(eastSt < minStorage)
+		{
+			minStorage = eastSt;
+		}
+	}
+
+	return minStorage;
+	fin.close();
+}
+
+
+// Returns the maximum storage in the East basin in 2018
+double get_max_east()
+{
+	double maxStorage = INT_MIN;
+	
+	std::string d;
+	double eastSt;
+	double eastEl;
+	double westSt;
+	double westEl;
+
+	std::ifstream fin("Current_Reservoir_Levels.tsv");
+	if(fin.fail())
+	{
+		std::cerr << "File cannot be opened for reading." << std::endl;
+		exit(1);	// exit if failed to open the file
+	}
+
+	std::string junk;	// new string variable
+	getline(fin, junk);	// read one line from the file
+  
+  	while(fin >> d >> eastSt >> eastEl >> westSt >> westEl)
+	{
+			// this loop reads the file line-by-line
+			// extracting 5 values on each iteration
+
+		fin.ignore(INT_MAX, '\n');	// skips to the end of line,
+													// ignoring the remaining columns
+
+		if(eastSt > maxStorage)
+		{
+			maxStorage = eastSt;
+		}
+	}
+
+	return maxStorage;
+	fin.close();
+}
