3df98e612be566e6e0aaad89338bc959e3203742
Ashlee Tolentino <ashleetolentino@Ashlees-Air.lan>
2022-09-17 01:08:32 -0400

    task B done

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 Author: Ashlee Tolentino
 Course: CSCI-135
 Instructor: Michael Zamansky
-Assignment: Lab3A
+Assignment: Lab3B
 
 This program accepts a std::string specifying a date and it returns the East Basin storage for that day.
 */
@@ -20,6 +20,12 @@ int main()
 {
   double east_storage = get_east_storage("05/20/2018");
   std::cout << "East basin storage: " << east_storage << " billion gallons" << std::endl;
+
+	double min_east_storage = get_min_east();
+	std::cout << "Minimum storage in East basin: " << min_east_storage << " billion gallons" << std::endl;
+
+	double max_east_storage = get_max_east();
+	std::cout << "Maximum storage in East basin: " << max_east_storage << " billion gallons" << std::endl;
  
-  return 0;
+	return 0;
 }
